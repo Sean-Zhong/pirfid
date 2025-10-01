a5b3351c8ec2735e7a4b52ebdd007479fa7b124d
Sean Zhong <sean.zhong98@gmail.com>
2025-10-01 20:58:39 +0200

    Feature: Use first 4 hex bytes as id

--- read_rfid.py
+++ read_rfid.py
@@ -13,20 +13,19 @@ server_url = "http://192.168.111.98:5000/cast"
 while True:
     try:
         print("Place your RFID tag near the reader...")
-        id, text = reader.read()
-        print(f"Tag ID: {id}")
-
-        # Send the RFID tag ID to the home server
-        response = requests.post(server_url, json={"card_id": str(id)})
+        full_id_decimal, text = reader.read()
+        full_id_hex = hex(full_id_decimal)[2:]
+        first_4_bytes_hex = full_id_hex[:8]
+        response = requests.post(server_url, json={"card_id": first_4_bytes_hex})
 
         if response.status_code == 200:
             print("Successfully sent command to server.")
         else:
             print(f"Failed to send command. Server responded with status code: {response.status_code}")
+            print(f"Response text: {response.text}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
-    
-    time.sleep(2)  # Add a small delay to prevent rapid-fire requests
+    time.sleep(2)
 
 GPIO.cleanup()
